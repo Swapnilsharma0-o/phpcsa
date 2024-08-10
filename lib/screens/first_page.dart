cd3fff4a83f967cdc518b1c0ca5dd4b283d2bc35
ankur0002 <ankurjadhav2000@gmail.com>
2024-08-10 17:20:22 +0530

    second day commit

--- lib/screens/first_page.dart
+++ lib/screens/first_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:process_run/shell.dart';
 import 'package:yaru/yaru.dart';
@@ -12,10 +14,15 @@ class FirstPage extends StatefulWidget {
 class _FirstPageState extends State<FirstPage> {
   final _form = GlobalKey<FormState>();
   var _clusterName = TextEditingController();
+  var _clusterDesc = TextEditingController();
+  var _cname;
+  var _cdesc;
   var shell = Shell();
 
   @override
   void initState() {
+    Directory.current = '/';
+
     // TODO: implement initState
     super.initState();
 
@@ -29,18 +36,31 @@ pwd
 ''');
   }
 
-  void write() {
-    shell.run(
-        ''' touch /home/ubuntu/Desktop/project/phpcsa-master/cluster_info.txt 
-    ${_clusterName.text.toString()} > /home/ubuntu/Desktop/project/phpcsa-master/cluster_info.txt 
-    pwd ''');
+  write() {
+    shell.run("mkdir -p /home/ubuntu/Cluster");
+    Directory.current = '/home/ubuntu/Cluster';
+    // shell.runSync("cd ~/Cluster");
+    shell.run("pwd");
+    _cname = _clusterName.text;
+    _cdesc = _clusterDesc.text;
+
+    Process.run(
+      'sh',
+      ['-c', 'echo "name: $_cname\ndecription: $_cdesc" > cluster_info.txt'],
+    );
+
+    // var process = await Process.start("cat", ['cluster_info.txt']);
+    // process.stdin
+    //     .writeln('''Hello, world!\n ${_clusterName}\n${_clusterDesc}''');
+
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           // Retrieve the text the that user has entered by using the
           // TextEditingController.
-          content: Text('''${_clusterName.text} written to cluster_info'''),
+          content: Text(
+              '''${_clusterName.text.toString()} and ${_clusterDesc.text.toString()} written to cluster_info'''),
         );
       },
     );
@@ -49,7 +69,7 @@ pwd
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Expanded(
         child: Column(
           children: [
             Image.asset(
@@ -59,34 +79,62 @@ pwd
             const SizedBox(
               height: 6,
             ),
-            const Text(
-              textAlign: TextAlign.center,
-              'will work here 😅',
-            ),
             Card(
+              margin: EdgeInsets.fromLTRB(400, 0, 400, 0),
               color: Colors.white,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
-                  mainAxisSize: MainAxisSize.min,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text("Enter Cluster Name"),
-                    Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: TextField(
-                        decoration: const InputDecoration(
-                          hintText: "Cluster",
-                          fillColor: Colors.white10,
-                          filled: true,
-                        ),
-                        controller: _clusterName,
-                      ),
+                    const Text(
+                      "Enter Cluster Name",
+                      textAlign: TextAlign.left,
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        write();
-                      },
-                      child: const Text("submit"),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    TextField(
+                      decoration: const InputDecoration(
+                        hintText: "Cluster",
+                        fillColor: Colors.white10,
+                        filled: true,
+                      ),
+                      controller: _clusterName,
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const Text(
+                      "Enter Cluster Description",
+                      textAlign: TextAlign.left,
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    TextField(
+                      decoration: const InputDecoration(
+                        hintText: "Description",
+                        fillColor: Colors.white10,
+                        filled: true,
+                      ),
+                      controller: _clusterDesc,
+                      keyboardType: TextInputType.multiline,
+                      minLines: 1, //Normal textInputField will be displayed
+                      maxLines: 5,
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          write();
+                        },
+                        child: const Text("submit"),
+                      ),
                     ),
                   ],
                 ),
